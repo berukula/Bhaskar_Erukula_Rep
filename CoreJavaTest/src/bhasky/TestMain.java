97a8ac40c7843e20cd6ae3acb7c384ae3e8d2dfc
berukula <bhaskar.erukula@hotmail.com>
2017-07-06 18:36:09 +0530

    commiting my first files to git

---
+++ CoreJavaTest/src/bhasky/TestMain.java
@@ -0,0 +1,20 @@
+package bhasky;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Maven - Git - Testing");	
+		System.out.println(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
+		System.out.println(new Date());	
+	}
+
+}
